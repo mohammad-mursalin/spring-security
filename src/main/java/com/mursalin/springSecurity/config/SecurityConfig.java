eac44a3850feb69547446a5c32e88e262d081034
mohammad-mursalin <mursalinlamon@gmail.com>
2024-10-07 11:58:38 +0600

    customizing the authentication provider or UseDetailsService

--- src/main/java/com/mursalin/springSecurity/config/SecurityConfig.java
+++ src/main/java/com/mursalin/springSecurity/config/SecurityConfig.java
@@ -1,15 +1,21 @@
 package com.mursalin.springSecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
@@ -18,5 +24,16 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails userDetails1 = User.withDefaultPasswordEncoder().username("lamon").password("4321").roles("user").build();
+
+        UserDetailsService userDetailsService = new InMemoryUserDetailsManager(userDetails1);
+
+        return userDetailsService;
+
     }
 }
