8b6b805416328a21e181d25e82297e0a9f94841c
mohammad-mursalin <mursalinlamon@gmail.com>
2024-10-07 11:39:41 +0600

    customizing filter chain

--- src/main/java/com/mursalin/springSecurity/config/SecurityConfig.java
+++ src/main/java/com/mursalin/springSecurity/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.mursalin.springSecurity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        return http.build();
+        return http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
